adbea80913950f1083829fb99098cc5018d60a35
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-06-01 11:42:40 -0500

    more progress on setup

--- moves_improved.c
+++ moves_improved.c
@@ -2,6 +2,8 @@
 
 #define BOARD_SIZE 64
 #define VIRTUAL_BOARD_SIZE 120
+// for use in sq64to120
+#define BOARD_TO_VIRT_BOARD_OFFSET 21
 
 typedef enum { WHITE, BLACK } SIDE;
 enum {
@@ -21,9 +23,33 @@ typedef struct {
 	int castlePermission;
 } BOARD_STATE;
 
+// translate up, right, down, left, etc.
+enum {
+	TUP =  10,
+	TRT =   1,
+	TDN = -10,
+	TLF =  -1,
+	TNW =   9,
+	TNE =  11,
+	TSE =  -9,
+	TSW = -11,
+};
+const int numDirections[] = {8, 4, 4, 8, 8};
+const int translation[][8] = {
+	{-21, -12, 8, 19, 21, 12, -8, -19}, // knights 0
+	{TSW, TNW, TNE, TSE}, // bishops TRT
+	{TDN, TLF, TUP, TRT}, // rooks 2
+	{TSW, TNW, TNE, TSE, TDN, TLF, TUP, TRT}, // queens 3
+	{TSW, TNW, TNE, TSE, TDN, TLF, TUP, TRT}  // kings 4
+};
+
 char sliders[2][BOARD_SIZE], contact[2][BOARD_SIZE];
 // should these be arrays of pointers?
 char last_slider[2], last_knight[2];
+
+int sq64to120(int sq64){
+	return sq64 + BOARD_TO_VIRT_BOARD_OFFSET + 2 * (sq64 - sq64 % 8) / 8;
+}
 
 void init(BOARD_STATE *bs){
 	for(int i = 0 ; i < VIRTUAL_BOARD_SIZE ; i++)
@@ -36,6 +62,8 @@ void init(BOARD_STATE *bs){
 void genLegalMovesImproved(BOARD_STATE *bs){
 	return;
 }
+
+extern int parseFEN(BOARD_STATE *bs, char *fen);
 
 void main(){
 	BOARD_STATE *bs = malloc(sizeof(BOARD_STATE));
@@ -50,3 +78,5 @@ void main(){
 	// print moves
 	// printLegalMoves(bs);
 }
+
+
